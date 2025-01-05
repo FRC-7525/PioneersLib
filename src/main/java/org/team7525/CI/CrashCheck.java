7200b534acc419998fa082389ab291b1e1c24efd
Otto <grentomato@gmail.com>
2025-01-05 02:51:13 -0600

    Add support for crash checking

--- src/main/java/org/team7525/CI/CrashCheck.java
+++ src/main/java/org/team7525/CI/CrashCheck.java
@@ -1,243 +1,229 @@
-// package org.team7525.CI;
+package org.team7525.CI;
 
-// TODO: See how to integrate, no robot.java file here so we don't really have the needed thing
+import edu.wpi.first.hal.DriverStationJNI;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.IterativeRobotBase;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import java.util.concurrent.atomic.AtomicReference;
 
-// import edu.wpi.first.hal.DriverStationJNI;
-// import edu.wpi.first.hal.HAL;
-// import edu.wpi.first.networktables.NetworkTableInstance;
-// import edu.wpi.first.wpilibj.DriverStation;
-// import edu.wpi.first.wpilibj.RobotBase;
-// import edu.wpi.first.wpilibj.simulation.DriverStationSim;
-// import frc.robot.Robot;
-// import java.util.concurrent.atomic.AtomicReference;
+public class CrashCheck extends IterativeRobotBase {
 
-// public class CrashCheck extends RobotBase {
+	private static IterativeRobotBase robot;
+	private final AtomicReference<CrashCheckStates> currentState = new AtomicReference<>(
+		CrashCheckStates.DISABLED
+	);
+	private final AtomicReference<CrashCheckStates> lastState = new AtomicReference<>(
+		CrashCheckStates.TEST
+	);
 
-// 	private static final AtomicReference<CrashCheck> instance = new AtomicReference<>();
-// 	private final Robot robot;
-// 	private final AtomicReference<CrashCheckStates> currentState = new AtomicReference<>(
-// 		CrashCheckStates.DISABLED
-// 	);
-// 	private final AtomicReference<CrashCheckStates> lastState = new AtomicReference<>(
-// 		CrashCheckStates.TEST
-// 	);
+	private long startTime = 0;
+	// Holy I hate this notation so much (Required as a flag)
+	private boolean m_robotMainOverridden;
+	private boolean lastStateSet = false;
 
-// 	private long startTime = 0;
-// 	// Holy I hate this notation so much (Required as a flag)
-// 	private boolean m_robotMainOverridden;
-// 	private boolean lastStateSet = false;
+	private enum CrashCheckStates {
+		DISABLED(
+			"DISABLED",
+			() -> {
+				getRobot().robotPeriodic();
+				getRobot().disabledPeriodic();
+			},
+			() -> {
+			    getRobot().disabledInit();
+			},
+			() -> {
+				getRobot().disabledExit();
+			}
+		),
+		TELEOP(
+			"TELEOP",
+			() -> {
+				getRobot().robotPeriodic();
+				getRobot().teleopPeriodic();
+			},
+			() -> {
+				getRobot().teleopInit();
+			},
+			() -> {
+				getRobot().teleopExit();
+			}
+		),
+		AUTONOMOUS(
+			"AUTONOMOUS",
+			() -> {
+				getRobot().robotPeriodic();
+				getRobot().autonomousPeriodic();
+			},
+			() -> {
+				getRobot().autonomousInit();
+			},
+			() -> {
+				getRobot().autonomousExit();
+			}
+		),
+		TEST(
+			"TEST",
+			() -> {
+				getRobot().robotPeriodic();
+				getRobot().testPeriodic();
+			},
+			() -> {
+				getRobot().testInit();
+			},
+			() -> {
+				getRobot().testExit();
+			}
+		);
 
-// 	private enum CrashCheckStates {
-// 		DISABLED(
-// 			"DISABLED",
-// 			() -> {
-// 				getInstance().getRobot().robotPeriodic();
-// 				getInstance().getRobot().disabledPeriodic();
-// 			},
-// 			() -> {
-// 				getInstance().getRobot().disabledInit();
-// 			},
-// 			() -> {
-// 				getInstance().getRobot().disabledExit();
-// 			}
-// 		),
-// 		TELEOP(
-// 			"TELEOP",
-// 			() -> {
-// 				getInstance().getRobot().robotPeriodic();
-// 				getInstance().getRobot().teleopPeriodic();
-// 			},
-// 			() -> {
-// 				getInstance().getRobot().teleopInit();
-// 			},
-// 			() -> {
-// 				getInstance().getRobot().teleopExit();
-// 			}
-// 		),
-// 		AUTONOMOUS(
-// 			"AUTONOMOUS",
-// 			() -> {
-// 				getInstance().getRobot().robotPeriodic();
-// 				getInstance().getRobot().autonomousPeriodic();
-// 			},
-// 			() -> {
-// 				getInstance().getRobot().autonomousInit();
-// 			},
-// 			() -> {
-// 				getInstance().getRobot().autonomousExit();
-// 			}
-// 		),
-// 		TEST(
-// 			"TEST",
-// 			() -> {
-// 				getInstance().getRobot().robotPeriodic();
-// 				getInstance().getRobot().testPeriodic();
-// 			},
-// 			() -> {
-// 				getInstance().getRobot().testInit();
-// 			},
-// 			() -> {
-// 				getInstance().getRobot().testExit();
-// 			}
-// 		);
+		private String state;
+		private Runnable periodic;
+		private Runnable init;
+		private Runnable exit;
 
-// 		private String state;
-// 		private Runnable periodic;
-// 		private Runnable init;
-// 		private Runnable exit;
+		CrashCheckStates(String state, Runnable periodic, Runnable init, Runnable exit) {
+			this.state = state;
+			this.periodic = periodic;
+			this.init = init;
+			this.exit = exit;
+		}
 
-// 		CrashCheckStates(String state, Runnable periodic, Runnable init, Runnable exit) {
-// 			this.state = state;
-// 			this.periodic = periodic;
-// 			this.init = init;
-// 			this.exit = exit;
-// 		}
+		public String getStateString() {
+			return state;
+		}
 
-// 		public String getStateString() {
-// 			return state;
-// 		}
+		public void periodic() {
+			periodic.run();
+		}
 
-// 		public void periodic() {
-// 			periodic.run();
-// 		}
+		public void init() {
+			init.run();
+		}
 
-// 		public void init() {
-// 			init.run();
-// 		}
+		public void exit() {
+			exit.run();
+		}
+	}
 
-// 		public void exit() {
-// 			exit.run();
-// 		}
-// 	}
+	public CrashCheck(IterativeRobotBase robot) {
+		super(0.02); 
+		CrashCheck.robot = robot;
+		HAL.initialize(500, 0);
 
-// 	public CrashCheck() {
-// 		this.robot = new Robot();
-// 		HAL.initialize(500, 0);
+		DriverStationSim.setEnabled(false);
+		DriverStationSim.setAutonomous(false);
+		DriverStationSim.setTest(false);
 
-// 		DriverStationSim.setEnabled(false);
-// 		DriverStationSim.setAutonomous(false);
-// 		DriverStationSim.setTest(false);
+		Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+			System.exit(1);
+		});
+	}
 
-// 		Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
-// 			System.exit(1);
-// 		});
-// 	}
+	public static IterativeRobotBase getRobot() {
+		return robot;
+	}
 
-// 	public Robot getRobot() {
-// 		return robot;
-// 	}
+	private void updateState() {
+		if (lastState.get() != currentState.get() && lastStateSet) {
+			if (lastState != null) {
+				lastState.get().exit();
+				System.out.println(lastState.get().getStateString() + " ran propperly.");
+			}
+			currentState.get().init();
+		}
+		lastState.set(currentState.get());
+		lastStateSet = true;
+		currentState.get().periodic();
+	}
 
-// 	public static CrashCheck getInstance() {
-// 		synchronized (CrashCheck.class) {
-// 			if (instance.get() == null) {
-// 				CrashCheck newInstance = new CrashCheck();
-// 				instance.set(newInstance);
-// 				return newInstance;
-// 			}
-// 			return instance.get();
-// 		}
-// 	}
+	private void setMode(CrashCheckStates newState) {
+		if (currentState.get() != newState) {
+			currentState.set(newState);
+			switch (newState) {
+				case DISABLED:
+					DriverStationSim.setEnabled(false);
+					break;
+				case TELEOP:
+					DriverStationSim.setEnabled(true);
+					DriverStationSim.setAutonomous(false);
+					DriverStationSim.setTest(false);
+					break;
+				case AUTONOMOUS:
+					DriverStationSim.setEnabled(true);
+					DriverStationSim.setAutonomous(true);
+					DriverStationSim.setTest(false);
+					break;
+				case TEST:
+					DriverStationSim.setEnabled(true);
+					DriverStationSim.setAutonomous(false);
+					DriverStationSim.setTest(true);
+					break;
+			}
+		}
+	}
 
-// 	private void updateState() {
-// 		if (lastState.get() != currentState.get() && lastStateSet) {
-// 			if (lastState != null) {
-// 				lastState.get().exit();
-// 				System.out.println(lastState.get().getStateString() + " ran propperly.");
-// 			}
-// 			currentState.get().init();
-// 		}
-// 		lastState.set(currentState.get());
-// 		lastStateSet = true;
-// 		currentState.get().periodic();
-// 	}
+	// L
+	@Override
+	public void startCompetition() {
+		robot.robotInit();
 
-// 	private void setMode(CrashCheckStates newState) {
-// 		if (currentState.get() != newState) {
-// 			currentState.set(newState);
-// 			switch (newState) {
-// 				case DISABLED:
-// 					DriverStationSim.setEnabled(false);
-// 					break;
-// 				case TELEOP:
-// 					DriverStationSim.setEnabled(true);
-// 					DriverStationSim.setAutonomous(false);
-// 					DriverStationSim.setTest(false);
-// 					break;
-// 				case AUTONOMOUS:
-// 					DriverStationSim.setEnabled(true);
-// 					DriverStationSim.setAutonomous(true);
-// 					DriverStationSim.setTest(false);
-// 					break;
-// 				case TEST:
-// 					DriverStationSim.setEnabled(true);
-// 					DriverStationSim.setAutonomous(false);
-// 					DriverStationSim.setTest(true);
-// 					break;
-// 			}
-// 		}
-// 	}
+		System.out.println("********** Robot program startup complete **********");
 
-// 	// L
-// 	@Override
-// 	public void startCompetition() {
-// 		robot.robotInit();
+		// Having to use this is actually crazy, massive skill issue ngl
+		startTime = System.nanoTime();
 
-// 		System.out.println("********** Robot program startup complete **********");
+		while (!m_robotMainOverridden) {
+			double testTime = (System.nanoTime() - startTime) / 1e9;
 
-// 		// Having to use this is actually crazy, massive skill issue ngl
-// 		startTime = System.nanoTime();
+			DriverStationSim.notifyNewData();
+			DriverStation.refreshData();
 
-// 		while (!m_robotMainOverridden) {
-// 			double testTime = (System.nanoTime() - startTime) / 1e9;
+			// Run test sequence
+			// System.out.println("Test Time: " + testTime);
 
-// 			DriverStationSim.notifyNewData();
-// 			DriverStation.refreshData();
+			// State Transitions
+			if (testTime < 2.0) {
+				setMode(CrashCheckStates.DISABLED);
+			} else if (testTime < 4.0) {
+				setMode(CrashCheckStates.TELEOP);
+			} else if (testTime < 6.0) {
+				setMode(CrashCheckStates.AUTONOMOUS);
+			} else if (testTime < 8.0) {
+				setMode(CrashCheckStates.TEST);
+			} else {
+				System.out.println(currentState.get().getStateString() + " ran propperly.");
+				System.out.println("Test completed after " + testTime + " seconds");
+				break;
+			}
 
-// 			// Run test sequence
-// 			// System.out.println("Test Time: " + testTime);
+			updateState();
 
-// 			// State Transitions
-// 			if (testTime < 2.0) {
-// 				setMode(CrashCheckStates.DISABLED);
-// 			} else if (testTime < 4.0) {
-// 				setMode(CrashCheckStates.TELEOP);
-// 			} else if (testTime < 6.0) {
-// 				setMode(CrashCheckStates.AUTONOMOUS);
-// 			} else if (testTime < 8.0) {
-// 				setMode(CrashCheckStates.TEST);
-// 			} else {
-// 				System.out.println(currentState.get().getStateString() + " ran propperly.");
-// 				System.out.println("Test completed after " + testTime + " seconds");
-// 				break;
-// 			}
+			if (currentState.get() == CrashCheckStates.DISABLED) {
+				DriverStationJNI.observeUserProgramDisabled();
+			} else if (currentState.get() == CrashCheckStates.TELEOP) {
+				DriverStationJNI.observeUserProgramTeleop();
+			} else if (currentState.get() == CrashCheckStates.AUTONOMOUS) {
+				DriverStationJNI.observeUserProgramAutonomous();
+			} else if (currentState.get() == CrashCheckStates.TEST) {
+				DriverStationJNI.observeUserProgramTest();
+			}
 
-// 			updateState();
+			NetworkTableInstance.getDefault().flushLocal();
 
-// 			if (currentState.get() == CrashCheckStates.DISABLED) {
-// 				DriverStationJNI.observeUserProgramDisabled();
-// 			} else if (currentState.get() == CrashCheckStates.TELEOP) {
-// 				DriverStationJNI.observeUserProgramTeleop();
-// 			} else if (currentState.get() == CrashCheckStates.AUTONOMOUS) {
-// 				DriverStationJNI.observeUserProgramAutonomous();
-// 			} else if (currentState.get() == CrashCheckStates.TEST) {
-// 				DriverStationJNI.observeUserProgramTest();
-// 			}
+			// This the periodic thingy or sum or other
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+				break;
+			}
+		}
+		System.out.println("********** Competition Ended **********");
+	}
 
-// 			NetworkTableInstance.getDefault().flushLocal();
-
-// 			// This the periodic thingy or sum or other
-// 			try {
-// 				Thread.sleep(20);
-// 			} catch (InterruptedException e) {
-// 				Thread.currentThread().interrupt();
-// 				break;
-// 			}
-// 		}
-// 		System.out.println("********** Competition Ended **********");
-// 	}
-
-// 	@Override
-// 	public void endCompetition() {
-// 		m_robotMainOverridden = true;
-// 	}
-// }
+	@Override
+	public void endCompetition() {
+		m_robotMainOverridden = true;
+	}
+}
