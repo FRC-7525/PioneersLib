0d38868923520c987ed96bae4425de4960126312
PotmanNob <Potmannob@outlook.com>
2025-03-20 20:58:17 -0500

    Reverts Tracer

--- src/main/java/org/team7525/misc/Tracer.java
+++ src/main/java/org/team7525/misc/Tracer.java
@@ -11,47 +11,104 @@ import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+/*
+ * Credit to @ohyes10fps from 3173
+ */
+
+/**
+ * A Utility class for tracing code execution time. Will put info to NetworkTables under the
+ * "Tracer" table.
+ *
+ * <p>Example inside {@code Robot.java}
+ *
+ * <pre><code>
+ *
+ * public void robotPeriodic() {
+ *   CommandScheduler.getInstance().run(); // CommandScheduler is already traced
+ *   Tracer.traceFunc("MyVendorDep", MyVendorDep::updateAll);
+ * }
+ * </code></pre>
+ *
+ * <p>Example inside a {@code Drive Subsystem}
+ *
+ * <pre><code>
+ * // Subsystem periodics are automaticall traced
+ * public void periodic() {
+ *   for (var module : modules) {
+ *     Tracer.traceFunc("Module" + module.getName(), module::update);
+ *   }
+ *   Tracer.traceFunc("Gyro", gyro::update);
+ * }
+ * </code></pre>
+ */
 public class Tracer {
   private static final class TraceStartData {
-    private double startTime;
-    private double totalGCTime;
-
+    private double m_startTime;
+    private double m_startGCTotalTime;
 
     private void set(double startTime, double startGCTotalTime) {
-      this.startTime = startTime;
-      this.totalGCTime = startGCTotalTime;
+      this.m_startTime = startTime;
+      this.m_startGCTotalTime = startGCTotalTime;
     }
   }
 
+  /**
+   * All of the tracers persistent state in a single object to be stored in a {@link ThreadLocal}.
+   */
   @SuppressWarnings("PMD.RedundantFieldInitializer")
   private static final class TracerState {
-    private final NetworkTable rootTable;
-    private final StringBuilder stackBuilder = new StringBuilder();
-    private final ArrayList<String> traceStack = new ArrayList<>();
+    private final NetworkTable m_rootTable;
 
-    private final ArrayList<String> traceStackHistory = new ArrayList<>();
+    /**
+     * The stack of traces, every startTrace will add to this stack and every endTrace will remove
+     * from this stack.
+     */
+    private final ArrayList<String> m_traceStack = new ArrayList<>();
 
-    private final HashMap<String, Double> traceTimes = new HashMap<>();
+    /**
+     * ideally we only need `traceStack` but in the interest of memory optimization and string
+     * concatenation speed we store the history of the stack to reuse the stack names.
+     */
+    private final ArrayList<String> m_traceStackHistory = new ArrayList<>();
 
-    private final HashMap<String, TraceStartData> TRACE_START_TIMES = new HashMap<>();
+    /** The time of each trace, the key is the trace name, modified every endTrace. */
+    private final HashMap<String, Double> m_traceTimes = new HashMap<>();
 
-    private final HashMap<String, TraceStartData> traceStartTimes = new HashMap<>();
+    /**
+     * The start time of each trace and the gc time at the start of the trace, the key is the trace
+     * name, modified every startTrace and endTrace.
+     */
+    private final HashMap<String, TraceStartData> m_traceStartTimes = new HashMap<>();
 
-    private final HashMap<String, DoublePublisher> publishers = new HashMap<>();
+    /** the publishers for each trace, the key is the trace name, modified every endCycle. */
+    private final HashMap<String, DoublePublisher> m_publishers = new HashMap<>();
 
+    /*
+     * If the cycle is poisoned, it will warn the user
+     * and not publish any data.
+     */
     boolean m_cyclePoisoned = false;
 
+    /** If the tracer is disabled, it will not publish any data or do any string manipulation. */
     boolean m_disabled = false;
 
+    /**
+     * If the tracer should be disabled next cycle and every cycle after that until this flag is set
+     * to false. Disabling is done this way to prevent disabling/enabling.
+     */
     boolean m_disableNextCycle = false;
 
+    /**
+     * Stack size is used to keep track of stack size even when disabled, calling `EndCycle` is
+     * important when disabled or not to update the disabled state in a safe manner.
+     */
     int m_stackSize = 0;
 
+    // the garbage collector beans
     private final ArrayList<GarbageCollectorMXBean> m_gcs =
         new ArrayList<>(ManagementFactory.getGarbageCollectorMXBeans());
     private final DoublePublisher m_gcTimeEntry;
@@ -66,29 +123,29 @@ public class Tracer {
       }
       anyTracesStarted.set(true);
       if (name == null) {
-        this.rootTable = NetworkTableInstance.getDefault().getTable("Tracer");
+        this.m_rootTable = NetworkTableInstance.getDefault().getTable("Tracer");
       } else {
-        this.rootTable = NetworkTableInstance.getDefault().getTable("Tracer").getSubTable(name);
+        this.m_rootTable = NetworkTableInstance.getDefault().getTable("Tracer").getSubTable(name);
       }
-      this.m_gcTimeEntry = rootTable.getDoubleTopic("GCTime").publish();
+      this.m_gcTimeEntry = m_rootTable.getDoubleTopic("GCTime").publish();
     }
 
     private String appendTraceStack(String trace) {
       m_stackSize++;
-
-      if (m_disabled) return "";
-
-      traceStack.add(trace);
-
-      if (!stackBuilder.isEmpty()) {
-        stackBuilder.append("/");
+      if (m_disabled) {
+        return "";
       }
-
-      stackBuilder.append(trace);
-
-      String result = stackBuilder.toString();
-      traceStackHistory.add(result);
-      return result;
+      m_traceStack.add(trace);
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < m_traceStack.size(); i++) {
+        sb.append(m_traceStack.get(i));
+        if (i < m_traceStack.size() - 1) {
+          sb.append('/');
+        }
+      }
+      String str = sb.toString();
+      m_traceStackHistory.add(str);
+      return str;
     }
 
     private String popTraceStack() {
@@ -96,20 +153,17 @@ public class Tracer {
       if (m_disabled) {
         return "";
       }
-      if (traceStack.isEmpty() || traceStackHistory.isEmpty() || m_cyclePoisoned) {
+      if (m_traceStack.isEmpty() || m_traceStackHistory.isEmpty() || m_cyclePoisoned) {
         m_cyclePoisoned = true;
         return "";
       }
-      traceStack.remove(traceStack.size() - 1);
-      return traceStackHistory.remove(traceStackHistory.size() - 1);
+      m_traceStack.remove(m_traceStack.size() - 1);
+      return m_traceStackHistory.remove(m_traceStackHistory.size() - 1);
     }
 
-    private final List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
-    private double cachedGC = 0.0;
-
     private double totalGCTime() {
-      double gcTime = cachedGC;
-      for (GarbageCollectorMXBean gc : gcs) {
+      double gcTime = 0;
+      for (GarbageCollectorMXBean gc : m_gcs) {
         gcTime += gc.getCollectionTime();
       }
       return gcTime;
@@ -117,26 +171,38 @@ public class Tracer {
 
     private void endCycle() {
       if (m_disabled != m_disableNextCycle || m_cyclePoisoned) {
-        publishers.forEach((key, pub) -> pub.set(0.0));
-      }
-
-      if (!m_disabled) {
-        for (var entry : traceTimes.entrySet()) {
-          DoublePublisher publisher = publishers.computeIfAbsent(
-                  entry.getKey(), key -> rootTable.getDoubleTopic(key).publish()
-          );
-          publisher.set(entry.getValue());
+        // Gives publishers empty times,
+        // reporting no data is better than bad data
+        for (var publisher : m_publishers.entrySet()) {
+          publisher.getValue().set(0.0);
         }
-
-        if (!gcs.isEmpty()) {
+        return;
+      } else if (!m_disabled) {
+        // update times for all already existing publishers
+        for (var publisher : m_publishers.entrySet()) {
+          Double time = m_traceTimes.remove(publisher.getKey());
+          if (time == null) {
+            time = 0.0;
+          }
+          publisher.getValue().set(time);
+        }
+        // create publishers for all new entries
+        for (var traceTime : m_traceTimes.entrySet()) {
+          DoublePublisher publisher = m_rootTable.getDoubleTopic(traceTime.getKey()).publish();
+          publisher.set(traceTime.getValue());
+          m_publishers.put(traceTime.getKey(), publisher);
+        }
+        // log gc time
+        if (!m_gcs.isEmpty()) {
           m_gcTimeEntry.set(m_gcTimeThisCycle);
         }
-
         m_gcTimeThisCycle = 0.0;
       }
 
-      traceTimes.clear();
-      traceStackHistory.clear();
+      // clean up state
+      m_traceTimes.clear();
+      m_traceStackHistory.clear();
+
       m_disabled = m_disableNextCycle;
     }
   }
@@ -145,13 +211,20 @@ public class Tracer {
   private static final AtomicBoolean anyTracesStarted = new AtomicBoolean(false);
   private static final ThreadLocal<TracerState> threadLocalState =
       ThreadLocal.withInitial(
-          () -> new TracerState(Thread.currentThread().getName(), true));
+          () -> {
+            return new TracerState(Thread.currentThread().getName(), true);
+          });
 
   private static void startTraceInner(final String name, final TracerState state) {
     String stack = state.appendTraceStack(name);
-    if (state.m_disabled) return;
-
-    TraceStartData data = state.traceStartTimes.computeIfAbsent(stack, k -> new TraceStartData());
+    if (state.m_disabled) {
+      return;
+    }
+    TraceStartData data = state.m_traceStartTimes.get(stack);
+    if (data == null) {
+      data = new TraceStartData();
+      state.m_traceStartTimes.put(stack, data);
+    }
     data.set(Timer.getFPGATimestamp() * 1_000.0, state.totalGCTime());
   }
 
@@ -165,31 +238,57 @@ public class Tracer {
             true);
         return;
       }
-      var startData = state.traceStartTimes.get(stack);
-      double gcTimeSinceStart = state.totalGCTime() - startData.totalGCTime;
+      var startData = state.m_traceStartTimes.get(stack);
+      double gcTimeSinceStart = state.totalGCTime() - startData.m_startGCTotalTime;
       state.m_gcTimeThisCycle += gcTimeSinceStart;
-      state.traceTimes.put(
-          stack, Timer.getFPGATimestamp() * 1_000.0 - startData.startTime - gcTimeSinceStart);
+      state.m_traceTimes.put(
+          stack, Timer.getFPGATimestamp() * 1_000.0 - startData.m_startTime - gcTimeSinceStart);
     }
-    if (state.traceStack.isEmpty()) {
+    if (state.m_traceStack.isEmpty()) {
       state.endCycle();
     }
   }
 
+  /**
+   * Starts a trace, should be called at the beginning of a function that's not being called by user
+   * code.
+   *
+   * @param name the name of the trace, should be unique to the function.
+   */
   public static void startTrace(String name) {
     startTraceInner(name, threadLocalState.get());
   }
 
+  /**
+   * Ends a trace, should only be called at the end of a function that's not being called by user
+   * code. If a {@link Tracer#startTrace(String)} is not paired with a {@link Tracer#endTrace()}
+   * there could be dropped or incorrect data.
+   */
   public static void endTrace() {
     endTraceInner(threadLocalState.get());
   }
 
+  /**
+   * Disables garbage collection logging for the current thread. This can help performance in some
+   * cases.
+   *
+   * <p>This counts as starting a tracer on the current thread, this is important to consider with
+   * {@link #enableSingleThreadedMode()} and should never be called before if you are using single
+   * threaded mode.
+   */
   public static void disableGcLoggingForCurrentThread() {
     TracerState state = threadLocalState.get();
     state.m_gcTimeEntry.close();
     state.m_gcs.clear();
   }
 
+  /**
+   * Enables single threaded mode for the Tracer. This will cause traces on different threads to
+   * throw an exception. This will shorten the path of traced data in NetworkTables by not including
+   * the thread name.
+   *
+   * <p>This function should be called before any traces are started.
+   */
   public static void enableSingleThreadedMode() {
     if (anyTracesStarted.get()) {
       DriverStation.reportError(
@@ -200,11 +299,23 @@ public class Tracer {
     }
   }
 
+  /**
+   * Disables any tracing for the current thread. This will cause all {@link #startTrace(String)},
+   * {@link #endTrace()} and {@link #traceFunc(String, Runnable)} to do nothing.
+   *
+   * <p>Being disabled prevents the {@link Tracer} from publishing any values to NetworkTables. This
+   * will cause all values to appear as if they're frozen at the value they were at when this
+   * function was called.
+   */
   public static void disableTracingForCurrentThread() {
     final TracerState state = threadLocalState.get();
     state.m_disableNextCycle = true;
   }
 
+  /**
+   * Enables any tracing for the current thread. This will cause all {@link #startTrace(String)},
+   * {@link #endTrace()} and {@link #traceFunc(String, Runnable)} to work as normal.
+   */
   public static void enableTracingForCurrentThread() {
     final TracerState state = threadLocalState.get();
     state.m_disableNextCycle = false;
@@ -213,6 +324,14 @@ public class Tracer {
     }
   }
 
+  /**
+   * Traces a function, should be used in place of {@link #startTrace(String)} and {@link
+   * #endTrace()} for functions called by user code like {@code CommandScheduler.run()} and other
+   * expensive functions.
+   *
+   * @param name the name of the trace, should be unique to the function.
+   * @param runnable the function to trace.
+   */
   public static void traceFunc(String name, Runnable runnable) {
     final TracerState state = threadLocalState.get();
     startTraceInner(name, state);
@@ -220,6 +339,15 @@ public class Tracer {
     endTraceInner(state);
   }
 
+  /**
+   * Traces a function, should be used in place of {@link #startTrace(String)} and {@link
+   * #endTrace()} for functions called by user code.
+   *
+   * @param <R> the return type of the function.
+   * @param name the name of the trace, should be unique to the function.
+   * @param supplier the function to trace.
+   * @return the return value of the function.
+   */
   public static <R> R traceFunc(String name, Supplier<R> supplier) {
     final TracerState state = threadLocalState.get();
     startTraceInner(name, state);
@@ -230,16 +358,28 @@ public class Tracer {
     }
   }
 
+  /** This function is only to be used in tests and is package private to prevent misuse. */
   static void resetForTest() {
     threadLocalState.remove();
     singleThreadedMode.set(false);
     anyTracesStarted.set(false);
   }
 
+  /**
+   * A class to help with substituting the current tracer state with a new one.
+   *
+   * <p>This can be used to act as if code is running on another thread even if it's running
+   * synchronously on the same thread.
+   */
   public static class SubstitutiveTracer {
     private final TracerState m_state;
     private final TracerState m_originalState;
 
+    /**
+     * Constructs a {@code SubstitutiveTracer} with the given name.
+     *
+     * @param name the name of the new tracer state.
+     */
     public SubstitutiveTracer(String name) {
       m_state = new TracerState(name, false);
       m_state.m_gcTimeEntry.close();
@@ -247,14 +387,21 @@ public class Tracer {
       m_originalState = threadLocalState.get();
     }
 
+    /** Substitutes the current tracer state with the new one. */
     public void subIn() {
       threadLocalState.set(m_state);
     }
 
+    /** Substitutes the current tracer state with the original one. */
     public void subOut() {
       threadLocalState.set(m_originalState);
     }
 
+    /**
+     * Runs the given runnable with the new tracer state.
+     *
+     * @param runnable the runnable to run.
+     */
     public void subWith(Runnable runnable) {
       subIn();
       try {
@@ -265,29 +412,62 @@ public class Tracer {
     }
   }
 
-  private static final long kMinPrintPeriod = 1000000;
+  // A REIMPLEMENTATION OF THE OLD TRACER TO NOT BREAK OLD CODE
 
-  private long m_lastEpochsPrintTime;
-  private long m_startTime;
+  private static final long kMinPrintPeriod = 1000000; // microseconds
 
-  private final HashMap<String, Long> m_epochs = new HashMap<>();
+  private long m_lastEpochsPrintTime; // microseconds
+  private long m_startTime; // microseconds
 
+  private final HashMap<String, Long> m_epochs = new HashMap<>(); // microseconds
+
+  /**
+   * Constructs a {@code Tracer} compatible with the 2024 {@code Tracer}.
+   *
+   * @deprecated This constructor is only for compatibility with the 2024 {@code Tracer} and will be
+   *     removed in 2025. Use the static methods in {@link Tracer} instead.
+   */
   @Deprecated(since = "2025", forRemoval = true)
   public Tracer() {
     resetTimer();
   }
 
+  /**
+   * Clears all epochs.
+   *
+   * @deprecated This method is only for compatibility with the 2024 {@code Tracer} and will be
+   *     removed in 2025. Use the static methods in {@link Tracer} instead.
+   */
   @Deprecated(since = "2025", forRemoval = true)
   public void clearEpochs() {
     m_epochs.clear();
     resetTimer();
   }
 
+  /**
+   * Restarts the epoch timer.
+   *
+   * @deprecated This method is only for compatibility with the 2024 {@code Tracer} and will be
+   *     removed in 2025. Use the static methods in {@link Tracer} instead.
+   */
   @Deprecated(since = "2025", forRemoval = true)
   public final void resetTimer() {
     m_startTime = RobotController.getFPGATime();
   }
 
+  /**
+   * Adds time since last epoch to the list printed by printEpochs().
+   *
+   * <p>Epochs are a way to partition the time elapsed so that when overruns occur, one can
+   * determine which parts of an operation consumed the most time.
+   *
+   * <p>This should be called immediately after execution has finished, with a call to this method
+   * or {@link #resetTimer()} before execution.
+   *
+   * @param epochName The name to associate with the epoch.
+   * @deprecated This method is only for compatibility with the 2024 {@code Tracer} and will be
+   *     removed in 2025. Use the static methods in {@link Tracer} instead.
+   */
   @Deprecated(since = "2025", forRemoval = true)
   public void addEpoch(String epochName) {
     long currentTime = RobotController.getFPGATime();
@@ -295,11 +475,26 @@ public class Tracer {
     m_startTime = currentTime;
   }
 
+  /**
+   * Prints list of epochs added so far and their times to the DriverStation.
+   *
+   * @deprecated This method is only for compatibility with the 2024 {@code Tracer} and will be
+   *     removed in 2025. Use the static methods in {@link Tracer} instead.
+   */
   @Deprecated(since = "2025", forRemoval = true)
   public void printEpochs() {
     printEpochs(out -> DriverStation.reportWarning(out, false));
   }
 
+  /**
+   * Prints list of epochs added so far and their times to the entered String consumer.
+   *
+   * <p>This overload can be useful for logging to a file, etc.
+   *
+   * @param output the stream that the output is sent to
+   * @deprecated This method is only for compatibility with the 2024 {@code Tracer} and will be
+   *     removed in 2025. Use the static methods in {@link Tracer} instead.
+   */
   @Deprecated(since = "2025", forRemoval = true)
   public void printEpochs(Consumer<String> output) {
     long now = RobotController.getFPGATime();
